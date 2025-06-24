d48976f3c6704264afef61687eaa4cb29a0f6723
Felipe <fd.duarte.b@gmail.com>
2025-06-24 14:30:32 -0300

    refact: create constant for app routes

--- src/renderer/src/components/Layout/useLayout.tsx
+++ src/renderer/src/components/Layout/useLayout.tsx
@@ -1,43 +1,43 @@
+import { APP_ROUTES } from '@renderer/core/constants/appRoutes'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { SidebarItem } from '../Sidebar/interfaces'
+import ContainerIcon from '../icons/ContainerIcon'
 import HomeIcon from '../icons/HomeIcon'
 import RadarIcon from '../icons/RadarIcon'
-import ContainerIcon from '../icons/ContainerIcon'
 import SettingsIcon from '../icons/SettingsIcon'
-import { routes } from '@renderer/pages/routes'
 
 export const useLayout = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  const isAnalysisPage = location.pathname === routes.find((route) => route.id === 'analysis')?.path
+  const isAnalysisPage = location.pathname === APP_ROUTES.ANALYSIS
 
   const sidebarItems: SidebarItem[] = [
     {
       icon: <HomeIcon className="fill-content-tertiary" />,
       label: 'Home',
       onClick: () => {
-        navigate('/')
+        navigate(APP_ROUTES.HOME)
       }
     },
     {
       icon: <RadarIcon className="fill-content-tertiary" />,
       label: 'Analysis',
       onClick: () => {
-        navigate('/analysis')
+        navigate(APP_ROUTES.ANALYSIS)
       }
     },
     {
       icon: <ContainerIcon className="fill-content-tertiary" />,
       label: 'Histórico',
       onClick: () => {
-        navigate('/history')
+        navigate(APP_ROUTES.HISTORY)
       }
     },
     {
       icon: <SettingsIcon className="fill-content-tertiary" />,
       label: 'Configurações',
       onClick: () => {
-        navigate('/settings')
+        navigate(APP_ROUTES.SETTINGS.USER)
       }
     }
   ]
