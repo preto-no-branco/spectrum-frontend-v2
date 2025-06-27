2a384f5415b99859b109fe63c605b8772f4b2c37
Felipe <fd.duarte.b@gmail.com>
2025-06-27 13:26:03 -0300

    fix: tooltip

--- src/renderer/src/pages/analysis/components/FiltersBar/ComplexFilters.tsx
+++ src/renderer/src/pages/analysis/components/FiltersBar/ComplexFilters.tsx
@@ -1,6 +1,6 @@
-import { Tooltip, TooltipContent, TooltipTrigger } from '@renderer/components/ui/tooltip'
-import { filter } from './interfaces'
+import { Tooltip } from '@renderer/components/ui/tooltip'
 import { cn } from '@renderer/lib/utils'
+import { filter } from './interfaces'
 
 export const ComplexFilters = ({
   filters,
@@ -13,16 +13,21 @@ export const ComplexFilters = ({
     <div className={cn('flex gap-2', className)}>
       {filters.map((filter) => {
         return (
-          <Tooltip delayDuration={500} key={filter.name}>
-            <TooltipTrigger
-              className="brightness-150 w-fit text-content-secondary cursor-pointer p-2 rounded hover:bg-background-tertiary transition-colors"
-              onClick={filter.function}
-            >
-              {filter.icon}
-            </TooltipTrigger>
-            <TooltipContent side="bottom">
-              <p className="text-content-secondary">{filter.name}</p>
-            </TooltipContent>
+          <Tooltip
+            delayDuration={500}
+            key={filter.name}
+            label={filter.name}
+            labelProps={{
+              side: 'bottom',
+              className: 'text-content-secondary'
+            }}
+            triggerProps={{
+              className:
+                'brightness-150 w-fit text-content-secondary cursor-pointer p-2 rounded hover:bg-background-tertiary transition-colors',
+              onClick: filter.function
+            }}
+          >
+            {filter.icon}
           </Tooltip>
         )
       })}
